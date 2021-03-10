f5959182068b40c2cf16e034e18bfda047d4add7
Mostakimul Karim <mk.mohit7574@gmail.com>
2021-03-10 01:54:39 +0100

    DcHero Vue Component Added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <app-header></app-header>
+  <div class="w-full flex">
+    <div class="m-auto">
+      <dc-heros></dc-heros>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DcHeros from "./components/DcHeros.vue";
+import AppHeader from "./components/AppHeader.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    DcHeros,
+    AppHeader,
+  },
+
+  data() {
+    return {
+      title: "Vue 3 Title",
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="container bg-red-500 py-4">
+    <nav class="grid grid-cols-2 place-items-center">
+      <div>
+        <h2 class="text-2xl font-semibold text-gray-100">Vue Apps</h2>
+      </div>
+      <div>
+        <ul class="flex space-x-4 text-white font-medium">
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+<style></style>
