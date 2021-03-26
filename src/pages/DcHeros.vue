4e6034ef5b507b61792fa2a52a290a32f0c9d1ed
Mostakimul Karim <mk.mohit7574@gmail.com>
2021-03-26 22:25:59 +0100

    Design Upgrade

--- src/pages/DcHeros.vue
+++ src/pages/DcHeros.vue
@@ -1,29 +1,35 @@
 <template>
-    <div class="w-full flex">
-        <div class="m-auto">
-            <div class="bg-gradient-to-tr from-blue-600 to-blue-300  mt-4 p-4">
-                <h3 class="text-3xl font-medium text-center mb-3">
-                    DC Heros ({{ totalHeros }})
-                </h3>
-                <ul class="hero mb-4">
-                    <li
-                        class="flex justify-between"
-                        v-for="(hero, index) in dcHeros"
-                        :key="hero.name"
-                    >
-                        <p>{{ hero.name }}</p>
-                        <button @click="removeHero(index)">x</button>
-                    </li>
-                </ul>
-                <form @submit.prevent="addHero">
-                    <input
-                        v-model.trim="newHero"
-                        placeholder="Enter your hero name"
-                    />
-                    <!-- trim, lazy (usefull for textarea), number -->
-                    <button type="submit">Add Hero</button>
-                </form>
-            </div>
+    <div class="w-1/2 bg-yellow-400 mx-auto text-center p-5 mt-6 rounded">
+        <h1 class="text-3xl font-medium text-center mb-3">
+            DC Heros ({{ totalHeros }})
+        </h1>
+        <div class="w-3/4 bg-gray-100 mx-auto rounded-lg">
+            <ul class=" p-4">
+                <li
+                    class="grid grid-cols-2 bg-gray-300 m-1 p-2 rounded-lg"
+                    v-for="(hero, index) in dcHeros"
+                    :key="hero.name"
+                >
+                    <p>{{ hero.name }}</p>
+                    <button @click="removeHero(index)">x</button>
+                </li>
+            </ul>
+        </div>
+        <div class="mt-4">
+            <form @submit.prevent="addHero">
+                <input
+                    class="p-2 rounded outline-none shadow-lg"
+                    v-model.trim="newHero"
+                    placeholder="Enter your hero name"
+                />
+                <!-- trim, lazy (usefull for textarea), number -->
+                <button
+                    class="ml-4 bg-gray-200 py-2 px-4 rounded shadow-lg hover:bg-gray-100 transition delay-150"
+                    type="submit"
+                >
+                    Add Hero
+                </button>
+            </form>
         </div>
     </div>
 </template>
